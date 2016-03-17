0bfdb0083b8627f8949fa6a116a9d1359c12a590
Felipe Henao <fhenaop@gmail.com>
2016-03-17 11:17:04 -0500

    index removed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'dreams#new'
-  resources :dreams
+  resources :dreams, except: [:index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
